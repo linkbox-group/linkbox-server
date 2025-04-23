fe27c260cbd1d26cf56c2824a437b1bce007b5a2
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2025-04-23 22:07:34 +0800

    fix: 没检查数据是否有效

--- app/organization/internal/service/create_organization.go
+++ app/organization/internal/service/create_organization.go
@@ -5,14 +5,27 @@ import (
 	"errors"
 	"fmt"
 	"github.com/linkbox-group/linkbox-server/model"
+	"github.com/linkbox-group/linkbox-server/model/treemodel"
+	"github.com/linkbox-group/linkbox-server/organization/pkg/log"
 )
 
 var (
 	ErrDbCreateTagFailed = errors.New("create tag failed")
 )
 
-func (s *OrganizationService) CreateOrganizationService(ctx context.Context, tag *model.Organization) (err error) {
-	err = s.repo.CreateOrganization(ctx, tag)
+func (s *OrganizationService) CreateOrganizationService(ctx context.Context, org *model.Organization) (err error) {
+	if org.ParentCode != "" || org.ParentCode != string(treemodel.ROOT_ID) {
+		log.Log().Debug("have parent")
+		_, err = s.repo.GetOrganization(ctx, org.ParentCode, org.UserID)
+
+	}
+	if err != nil {
+		log.Log().Error("未获取到父节点" + err.Error())
+		return ErrDbCreateTagFailed
+
+	}
+
+	err = s.repo.CreateOrganization(ctx, org)
 	if err != nil {
 		return fmt.Errorf("%w:%w", ErrDbCreateTagFailed, err)
 	}
