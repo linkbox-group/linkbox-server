eff6400865899acbaba6c22355077e934719f315
Xyq777 <140133095+Xyq777@users.noreply.github.com>
2025-04-28 03:35:23 +0800

    feat: 更改数据库存储的枚举类型的类型
    1.枚举类型在存储中应使用开销更小的选择

--- model/item.go
+++ model/item.go
@@ -2,18 +2,19 @@ package model
 
 import (
 	"github.com/linkbox-group/linkbox-server/model/array"
+	itemmodel "github.com/linkbox-group/linkbox-server/rpc-gen/model"
 	"gorm.io/gorm"
 )
 
 // 项目模型
 type Item struct {
 	BaseModel
-	UserID           string `gorm:"type:varchar(36);not null;index:idx_user_id;comment:用户ID" json:"userId"`
-	ItemType         string `gorm:"type:varchar(20);not null;index:idx_type;comment:类型:text,image,link,bookmark" json:"itemType"`
-	Title            string `gorm:"type:varchar(500);comment:标题" json:"title,omitempty"`
-	Note             string `gorm:"type:text;comment:内容/文本" json:"note,omitempty"`
-	URL              string `gorm:"type:varchar(2000);comment:链接地址" json:"url,omitempty"`
-	ThumbnailURL     string `gorm:"type:varchar(2000);comment:缩略图地址" json:"thumbnailUrl,omitempty"`
+	UserID           string             `gorm:"type:varchar(36);not null;index:idx_user_id;comment:用户ID" json:"userId"`
+	ItemType         itemmodel.ItemType `gorm:"type:varchar(20);not null;index:idx_type;comment:类型:text,image,link,bookmark" json:"itemType"`
+	Title            string             `gorm:"type:varchar(500);comment:标题" json:"title,omitempty"`
+	Note             string             `gorm:"type:text;comment:内容/文本" json:"note,omitempty"`
+	URL              string             `gorm:"type:varchar(2000);comment:链接地址" json:"url,omitempty"`
+	ThumbnailURL     string             `gorm:"type:varchar(2000);comment:缩略图地址" json:"thumbnailUrl,omitempty"`
 	TagNames         array.Array
 	OrganizationPath string
 
